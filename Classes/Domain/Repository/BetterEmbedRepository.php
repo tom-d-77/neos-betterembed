d811e1b5d985dd2f86773e279e979bcc60a7a99b
thomas.dingeldey <thomas.dingeldey@camao.one>
2025-09-03 14:43:10 +0200

    NodeService, BetterEmbedRepository, Neos.Settings - adjustments for Neos 9

--- Classes/Domain/Repository/BetterEmbedRepository.php
+++ Classes/Domain/Repository/BetterEmbedRepository.php
@@ -2,15 +2,7 @@
 
 namespace BetterEmbed\NeosEmbed\Domain\Repository;
 
-use Neos\Flow\Annotations as Flow;
-use Neos\ContentRepository\Domain\Model\NodeData;
-use Neos\ContentRepository\Domain\Repository\NodeDataRepository;
-
-/**
- * @Flow\Scope("singleton")
- */
-class BetterEmbedRepository extends NodeDataRepository
+final class BetterEmbedRepository
 {
-    const ENTITY_CLASSNAME = NodeData::class;
     const BETTER_EMBED_ROOT_NODE_NAME = 'better-embeds';
 }
--- Classes/Service/NodeService.php
+++ Classes/Service/NodeService.php
@@ -2,18 +2,16 @@
 
 namespace BetterEmbed\NeosEmbed\Service;
 
-use BetterEmbed\NeosEmbed\Domain\Repository\BetterEmbedRepository;
 use Doctrine\Common\Collections\ArrayCollection;
-use Neos\ContentRepository\Domain\Factory\NodeFactory;
-use Neos\ContentRepository\Domain\Model\NodeInterface;
-use Neos\ContentRepository\Domain\Model\NodeTemplate;
-use Neos\ContentRepository\Domain\Repository\NodeDataRepository;
-use Neos\ContentRepository\Domain\Service\Context;
-use Neos\ContentRepository\Domain\Service\ContextFactoryInterface;
-use Neos\ContentRepository\Domain\Service\NodeTypeManager;
+use Neos\ContentRepository\Core\Feature\NodeRemoval\Command\RemoveNodeAggregate;
+
+
+use Neos\ContentRepository\Core\SharedModel\Exception\NodeAggregateCurrentlyDoesNotExist;
+use Neos\ContentRepository\Core\SharedModel\Exception\NodeAggregateDoesCurrentlyNotCoverDimensionSpacePoint;
+use Neos\ContentRepository\Core\SharedModel\Node\NodeVariantSelectionStrategy;
+//use Neos\ContentRepository\Domain\Factory\NodeFactory;
 use Neos\Eel\Exception;
 use Neos\Eel\FlowQuery\FlowQuery;
-use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Persistence\Exception\IllegalObjectTypeException;
 use Neos\Flow\Persistence\PersistenceManagerInterface;
 use Neos\Media\Domain\Model\AssetCollection;
@@ -21,30 +19,20 @@ use Neos\Media\Domain\Model\Tag;
 use Neos\Media\Domain\Repository\AssetCollectionRepository;
 use Neos\Media\Domain\Repository\TagRepository;
 use phpDocumentor\Reflection\Types\Boolean;
+use Neos\ContentRepository\Core\SharedModel\Workspace\WorkspaceName;
+use Neos\Flow\Annotations as Flow;
 
-/**
- * @Flow\Scope("singleton")
- */
+#[Flow\Scope("singleton")]
 class NodeService
 {
 
     const ASSET_COLLECTION_TITLE = 'BetterEmbed';
 
-    /**
-     * @Flow\Inject
-     * @var BetterEmbedRepository
-     */
-    protected $betterEmbedRepository;
 
     /**
-     * @var \Neos\ContentRepository\Core\Projection\ContentGraph\Node
-     */
-    protected $betterEmbedRootNode;
-
-    /**
-     * @Flow\Inject
      * @var PersistenceManagerInterface
      */
+    #[Flow\Inject]
     protected $persistenceManager;
 
     /**
@@ -58,51 +46,9 @@ class NodeService
      * @var TagRepository
      */
     protected $tagRepository;
-    #[\Neos\Flow\Annotations\Inject]
+
+    #[Flow\Inject]
     protected \Neos\ContentRepositoryRegistry\ContentRepositoryRegistry $contentRepositoryRegistry;
-
-    /**
-     * @param \Neos\Rector\ContentRepository90\Legacy\LegacyContextStub $context
-     *
-     * @return \Neos\ContentRepository\Core\Projection\ContentGraph\Node
-     */
-    public function findOrCreateBetterEmbedRootNode(\Neos\Rector\ContentRepository90\Legacy\LegacyContextStub $context)
-    {
-
-        if ($this->betterEmbedRootNode instanceof \Neos\ContentRepository\Core\Projection\ContentGraph\Node) {
-            return $this->betterEmbedRootNode;
-        }
-
-        $betterEmbedRootNodeData = $this->betterEmbedRepository->findOneByPath('/' . BetterEmbedRepository::BETTER_EMBED_ROOT_NODE_NAME, $context->getWorkspace());
-
-        if ($betterEmbedRootNodeData !== null) {
-            $this->betterEmbedRootNode = $this->nodeFactory->createFromNodeData($betterEmbedRootNodeData, $context);
-
-            return $this->betterEmbedRootNode;
-        }
-        // TODO 9.0 migration: !! NodeTemplate is removed in Neos 9.0. Use the "CreateNodeAggregateWithNode" command to create new nodes or "CreateNodeVariant" command to create variants of an existing node in other dimensions.
-
-
-        $nodeTemplate = new NodeTemplate();
-        // TODO 9.0 migration: Make this code aware of multiple Content Repositories.
-        $contentRepository = $this->contentRepositoryRegistry->get(\Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryId::fromString('default'));
-        $nodeTemplate->setNodeType($contentRepository->getNodeTypeManager()->getNodeType('unstructured'));
-        // TODO 9.0 migration: !! NodeTemplate::setName is removed in Neos 9.0. Use the "CreateNodeAggregateWithNode" command to create new nodes or "CreateNodeVariant" command to create variants of an existing node in other dimensions.
-
-        $nodeTemplate->setName(BetterEmbedRepository::BETTER_EMBED_ROOT_NODE_NAME);
-        // TODO 9.0 migration: !! MEGA DIRTY CODE! Ensure to rewrite this; by getting rid of LegacyContextStub.
-        $contentRepository = $this->contentRepositoryRegistry->get(\Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryId::fromString('default'));
-        $workspace = $contentRepository->findWorkspaceByName(\Neos\ContentRepository\Core\SharedModel\Workspace\WorkspaceName::fromString($context->workspaceName ?? 'live'));
-        $rootNodeAggregate = $contentRepository->getContentGraph($workspace->workspaceName)->findRootNodeAggregateByType(\Neos\ContentRepository\Core\NodeType\NodeTypeName::fromString('Neos.Neos:Sites'));
-        $subgraph = $contentRepository->getContentGraph($workspace->workspaceName)->getSubgraph(\Neos\ContentRepository\Core\DimensionSpace\DimensionSpacePoint::fromLegacyDimensionArray($context->dimensions ?? []), $context->invisibleContentShown ? \Neos\ContentRepository\Core\Projection\ContentGraph\VisibilityConstraints::withoutRestrictions() : \Neos\ContentRepository\Core\Projection\ContentGraph\VisibilityConstraints::default());
-
-        $rootNode = $subgraph->findNodeById($rootNodeAggregate->nodeAggregateId);
-
-        $this->betterEmbedRootNode = $rootNode->createNodeFromTemplate($nodeTemplate);
-        $this->betterEmbedRepository->persistEntities();
-
-        return $this->betterEmbedRootNode;
-    }
 
     /**
      * @param string $title
@@ -185,16 +131,20 @@ class NodeService
         return $result;
     }
 
-    public function removeEmbedNode(\Neos\ContentRepository\Core\Projection\ContentGraph\Node $node)
+    public function removeEmbedNode(\Neos\ContentRepository\Core\Projection\ContentGraph\Node $node):void
     {
         // TODO 9.0 migration: !! Node::setRemoved() is not supported by the new CR. Use the "RemoveNodeAggregate" command to remove a node.
+        $contentRepository = $this->contentRepositoryRegistry->get($node->contentRepositoryId);
 
-        $node->setRemoved(true);
-        // TODO 9.0 migration: !! Node::isRemoved() - the new CR *never* returns removed nodes; so you can simplify your code and just assume removed == FALSE in all scenarios.
-
-        if ($node->isRemoved()) {
-            $this->nodeDataRepository->remove($node);
-            return;
+        try {
+            $contentRepository->handle(RemoveNodeAggregate::create(
+                WorkspaceName::forLive(),
+                $node->aggregateId,
+                $node->originDimensionSpacePoint->toDimensionSpacePoint(),
+                NodeVariantSelectionStrategy::STRATEGY_ALL_VARIANTS
+            ));
+        } catch (NodeAggregateCurrentlyDoesNotExist | NodeAggregateDoesCurrentlyNotCoverDimensionSpacePoint) {
+            // already removed by another command further up the graph
         }
     }
 }
